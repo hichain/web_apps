540e0440627440aee0d5f91021578c0370986de6
kiito1000 <dkmt402.nt@live.jp>
2020-06-10 21:24:43 +0900

    manage turns

--- src/Game.tsx
+++ src/Game.tsx
@@ -7,7 +7,9 @@ type GameState = {
 
 export const TicTacToe: Game<GameState> = {
   setup: () => ({ cells: Array(9).fill(null) }),
-
+  turn: {
+    moveLimit: 1,
+  },
   moves: {
     clickCell: (G, ctx, id) => {
       if (G.cells[id] !== null) {

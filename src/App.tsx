3f0f3137e6b8174cfeab188691b026e088698fed
kiito1000 <dkmt402.nt@live.jp>
2020-06-21 19:49:24 +0900

    create multiplayer server

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { Client } from "boardgame.io/react";
 import { TicTacToe } from "./Game";
 import { TicTacToeBoard } from "./Board";
+import { SocketIO } from "boardgame.io/multiplayer";
 import "./App.css";
+
+const serverUri = process.env.REACT_APP_DOMAIN;
 
 const App = Client({
   game: TicTacToe,
   board: TicTacToeBoard,
+  multiplayer: SocketIO({ server: serverUri })
 });
 
 export default App;

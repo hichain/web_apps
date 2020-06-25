0f091659a68388fedfe6847f61f9904c4551e7cb
kiito1000 <dkmt402.nt@live.jp>
2020-06-25 21:35:15 +0900

    define player's move

--- src/slashchain/board.ts
+++ src/slashchain/board.ts
@@ -1,6 +1,31 @@
-import { LineCell, TileCell } from "./components";
+import { LineCell, TileCell, Tile } from "./components";
 
 export class Board {
-	lineCells: Array<LineCell> = [];
-	tileCells: Array<TileCell> = [ new TileCell(0, 0) ];
-};
+  tileCells: ReadonlyArray<TileCell> = [new TileCell(0, 0)];
+  private lineCells: ReadonlyArray<LineCell> = [];
+
+  put(cell: TileCell, tile: Tile) {
+    cell.tile = tile;
+    this.applyLines(cell);
+    this.addAdjacentCells(cell);
+  }
+
+  private applyLines(tileCell: TileCell) {
+    const newLineCells = tileCell.tile
+      .innerCells()
+      .map(
+        (cell) =>
+          new LineCell(cell.x + tileCell.x * 2, cell.y + tileCell.y * 2, cell)
+      );
+    this.lineCells = this.lineCells.concat(newLineCells);
+  }
+
+  private addAdjacentCells(cell: TileCell) {
+    const newCells = cell
+      .adjacentCells()
+      .filter(
+        (newCell) => !this.tileCells.some((cell) => newCell.equals(cell))
+      );
+    this.tileCells = this.tileCells.concat(newCells);
+  }
+}

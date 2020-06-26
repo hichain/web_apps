9d8c85c989ade1f5231cb7776b99216b9c8700be
kiito1000 <dkmt402.nt@live.jp>
2020-06-26 21:21:55 +0900

    setup game

---
+++ src/slashchain/view/game.tsx
@@ -0,0 +1,37 @@
+import { Game } from "boardgame.io";
+import { INVALID_MOVE } from "boardgame.io/core";
+import { PlayerHands } from "../hands";
+import { Board } from "../board.js";
+import { HandsSet } from "../rules.js";
+
+export type GameState = {
+  ruleName: string;
+  board: Board;
+  hands: { [key: string]: PlayerHands };
+};
+
+const handsSet = new HandsSet();
+
+export const Slashchain: Game<GameState> = {
+  setup: (): GameState => ({
+    ruleName: handsSet.rules.basic_3x.name,
+    board: new Board(),
+    hands: {
+      first: handsSet.rules.basic_3x.hands,
+      second: handsSet.rules.basic_3x.hands,
+    },
+  }),
+  turn: {
+    moveLimit: 1,
+  },
+  moves: {
+    clickCell: (G, ctx, id) => {
+      // TODO: define moves
+      return INVALID_MOVE;
+    },
+  },
+  endIf: (G, ctx) => {
+    // TODO: define victory conditions
+    return { draw: true };
+  },
+};

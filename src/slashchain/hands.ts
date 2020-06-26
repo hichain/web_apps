6ca3e27481a13f17d64c8e5902d829a7be5229c8
kiito1000 <dkmt402.nt@live.jp>
2020-06-26 20:03:36 +0900

    define player's hands

---
+++ src/slashchain/hands.ts
@@ -0,0 +1,21 @@
+import { Tile } from "./components.js";
+
+class PlayerHands {
+  readonly tiles: Tile[];
+
+  constructor(tiles: Tile[]) {
+    this.tiles = tiles;
+  }
+
+  pick(tile: Tile) {
+    this.remove(tile, this.tiles)
+  }
+
+  private remove<T>(item: T, array: T[]) {
+		const index = array.findIndex((i) => i === item)
+		if (index !== -1) {
+			return;
+		}
+		array.splice(index, 1)
+	}
+}

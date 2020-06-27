61088b368decedc36450387c17ae37906dfc1858
kiito1000 <dkmt402.nt@live.jp>
2020-06-28 02:52:24 +0900

    move a state: picked tile to component state

--- src/slashchain/hands.ts
+++ src/slashchain/hands.ts
@@ -2,19 +2,17 @@ import { Tile } from "./components";
 
 export class PlayerHands {
   readonly tiles: Tile[];
-  pickedTile?: Tile;
 
   constructor(tiles: Tile[]) {
     this.tiles = tiles;
   }
 
   pick(tile: Tile) {
-    const index = this.tiles.findIndex((tile) => tile === this.pickedTile);
+    const index = this.tiles.findIndex((item) => item === tile);
     if (index === -1) {
       return;
     }
 		this.tiles.splice(index, 1);
-		this.pickedTile = undefined
   }
 
   clone() {

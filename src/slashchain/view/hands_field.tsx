efe44a505481fd813e8306156be51d37b352a6f0
kiito1000 <dkmt402.nt@live.jp>
2020-06-28 01:41:08 +0900

    set hover and active animation

--- src/slashchain/view/hands_field.tsx
+++ src/slashchain/view/hands_field.tsx
@@ -12,24 +12,31 @@ export interface HandsFieldProps {
 }
 
 export class HandsFieldComponent extends React.Component<HandsFieldProps> {
-	private gameState = this.props.G;
+  private gameState = this.props.G;
 
   onClick(tile: Tile) {
-		this.props.G.hands[this.props.playerID].pickedTile = tile;
+    this.props.G.hands[this.props.playerID].pickedTile = tile;
   }
 
   render() {
-		let clickHandler: (tile: Tile) => void = () => {};
-		const isMyTurn = this.props.ctx.currentPlayer === this.props.myPlayerID;
-		const isMyField = this.props.myPlayerID === this.props.playerID;
-		if (isMyTurn && isMyField) {
+    let tileClass = style.tile;
+    let clickHandler: (tile: Tile) => void = () => {};
+    const isMyTurn = this.props.ctx.currentPlayer === this.props.myPlayerID;
+    const isMyField = this.props.myPlayerID === this.props.playerID;
+    if (isMyTurn && isMyField) {
       clickHandler = (tile: Tile) => this.onClick(tile);
+      tileClass += " " + style.pickable;
     }
-    const tileItems = this.gameState.hands[this.props.playerID].tiles.map(
-      (tile, i) => (
-        <img className={style.tile} src={tile.imageUrl} alt={tile.name} onClick={() => clickHandler(tile)} />
-      )
-    );
+    const tileItems = this.gameState.hands[
+      this.props.playerID
+    ].tiles.map((tile, i) => (
+      <img
+        className={tileClass}
+        src={tile.imageUrl}
+        alt={tile.name}
+        onClick={() => clickHandler(tile)}
+      />
+    ));
 
     return <div className={style.field}>{tileItems}</div>;
   }

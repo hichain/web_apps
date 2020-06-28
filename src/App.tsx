355552c47d11f9902b887251a8c8454dc861fd06
kiito1000 <dkmt402.nt@live.jp>
2020-06-28 21:02:38 +0900

    set slashchain up on server

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Client } from "boardgame.io/react";
+import { SocketIO } from "boardgame.io/multiplayer";
 import "./App.css";
 import { Slashchain } from "./slashchain/game";
 import { GameComponent } from "./slashchain/view/game_state";
+
+const serverUri = process.env.REACT_APP_DOMAIN;
 
 const App = Client({
   game: Slashchain,
   numPlayers: 2,
   board: GameComponent,
+  multiplayer: SocketIO({ server: serverUri }),
 });
 
 export default App;
--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,20 +1,7 @@
 import { Server } from "boardgame.io/server";
-import path from "path";
-import serve from "koa-static";
-import { TicTacToe } from "../tic-tac-toe/Game";
+import { Slashchain } from "../slashchain/game";
 
-const server = Server({ games: [TicTacToe] });
+const server = Server({ games: [Slashchain] });
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
 
-const frontEndAppBuildPath = path.resolve(__dirname, "../../build");
-server.app.use(serve(frontEndAppBuildPath));
-
-server.run(PORT, () => {
-  server.app.use(
-    async (ctx, next) =>
-      await serve(frontEndAppBuildPath)(
-        Object.assign(ctx, { path: "index.html" }),
-        next
-      )
-  );
-});
+server.run(PORT);

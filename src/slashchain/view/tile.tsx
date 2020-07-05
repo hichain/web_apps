7a18b1806452436595425bc5a6a5e6a9fc2eb7ce
kiito1000 <dkmt402.nt@live.jp>
2020-07-05 21:23:13 +0900

    create a component: tile
    fix css styles

---
+++ src/slashchain/view/tile.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import styleClass from "../styles/component.module.scss";
+import square from "../images/square.png";
+import arrow from "../images/arrow.png";
+import pin from "../images/pin.png";
+import cross from "../images/cross.png";
+import power from "../images/power.png";
+import parallel from "../images/parallel.png";
+import { Tile } from "../components.js";
+
+const images: { [key: string]: string } = {
+  square,
+  arrow,
+  pin,
+  cross,
+  power,
+  parallel,
+};
+
+export interface TileProps {
+	tile: Tile
+}
+
+export class TileComponent extends React.Component<TileProps> {
+  render() {
+    const tile = this.props.tile;
+    const image = images[tile.name];
+    const style: { [key: string]: string } = {
+      transform: `rotate(${90 * tile.rotateCount}deg)`,
+    };
+    if (image == null) {
+      return (
+				<div className={styleClass.tile} style={style}>
+          {tile.name}
+        </div>
+      );
+    } else {
+      return (
+        <img
+          src={image}
+					className={styleClass.tile}
+          style={style}
+          alt={tile.name}
+        />
+      );
+    }
+  }
+}

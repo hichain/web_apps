e73e98659184684e15219c1007e920bc3bf0e42f
kiito1000 <dkmt402.nt@live.jp>
2020-08-10 01:23:24 +0900

    test: rotate tile

---
+++ src/slashchain/tests/tiles.test.ts
@@ -0,0 +1,28 @@
+import { rotate } from "../tiles";
+
+describe("rotate tile", () => {
+  test("square (dir+1)", () => {
+    const tile = rotate(0x99, 1);
+    expect(tile).toBe(0x99);
+  });
+  test("square (dir+4)", () => {
+    const tile = rotate(0x99, 4);
+    expect(tile).toBe(0x99);
+  });
+  test("pin (dir+2)", () => {
+    const tile = rotate(0xa9, 2);
+    expect(tile).toBe(0x6a);
+  });
+  test("power (dir-1)", () => {
+    const tile = rotate(0x56, -1);
+    expect(tile).toBe(0x9a);
+  });
+  test("parallel (dir-6)", () => {
+    const tile = rotate(0xaa, -6);
+    expect(tile).toBe(0xaa);
+  });
+  test("square (dir+100)", () => {
+    const tile = rotate(0x99, 100);
+    expect(tile).toBe(0x99);
+  });
+});

0b1be1713cb97710d64ab6cebc3e1e45f1816739
kiito1000 <dkmt402.nt@live.jp>
2020-08-11 01:36:56 +0900

    test: parse tile

--- src/slashchain/tests/tiles.test.ts
+++ src/slashchain/tests/tiles.test.ts
@@ -1,4 +1,4 @@
-import { rotate } from "../tiles";
+import { rotate, tileParser } from "../tiles";
 
 describe("rotate tile", () => {
   test("square (dir+1)", () => {
@@ -24,5 +24,32 @@ describe("rotate tile", () => {
   test("square (dir+100)", () => {
     const tile = rotate(0x99, 100);
     expect(tile).toBe(0x99);
+  });
+});
+
+describe("parse tile", () => {
+  test("square", () => {
+    const tile = tileParser.parse(0x99);
+    const results = Array(4)
+      .fill(null)
+      .map((_, i) => ({
+        name: "square",
+        dir: i,
+      }));
+    expect(results).toContainEqual(tile);
+  });
+  test("pin (dir+1)", () => {
+    const tile = tileParser.parse(0x56);
+    expect(tile).toEqual({
+      name: "pin",
+      dir: 1,
+    });
+  });
+  test("power (dir+2)", () => {
+    const tile = tileParser.parse(0x95);
+    expect(tile).toEqual({
+      name: "power",
+      dir: 2,
+    });
   });
 });

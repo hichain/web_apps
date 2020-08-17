f49d49b5edd8550b48a06948ae5c825af3ee9a7b
kiito1000 <dkmt402.nt@live.jp>
2020-08-17 21:47:55 +0900

    set router for setting player ID

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Route } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Route path="/slashchain/0" render={() => <App playerID={"0"} />}></Route>
+      <Route path="/slashchain/1" render={() => <App playerID={"1"} />}></Route>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change

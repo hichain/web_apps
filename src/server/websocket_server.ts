4adcab16dae4f4afaff0f1f3eece5710692bf72f
kiito1000 <kiito.dev@gmail.com>
2020-10-03 00:31:15 +0900

    prepare master server

--- src/server/websocket_server.ts
+++ src/server/websocket_server.ts
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 const server = Server({ games: [Slashchain] });
 dotenv.config();
 
-const port = process.env.REACT_APP_SERVER_PORT;
+const port = process.env.REACT_APP_MASTER_PORT;
 if (port != null) {
   server.run(parseInt(port));
 }

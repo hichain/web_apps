ea5b79f7ccff914a9b99921abf5dedd0e464089d
kiito1000 <kiito.dev@gmail.com>
2020-10-05 22:34:39 +0900

    fix port

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import "./App.css";
 import { Slashchain } from "./slashchain/game";
 import { GameComponent } from "./slashchain/view/game_state";
 
+const port = process.env.PORT || process.env.REACT_APP_MASTER_DEFAULT_PORT;
 const App = Client({
   game: Slashchain,
   numPlayers: 2,
   board: GameComponent,
   multiplayer: SocketIO({
-    server: `${process.env.REACT_APP_MASTER_URL}:${process.env.REACT_APP_MASTER_PORT}`,
+    server: `${process.env.REACT_APP_MASTER_URL}:${port}`,
   }),
 });
 
--- src/server/websocket_server.ts
+++ src/server/websocket_server.ts
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 const server = Server({ games: [Slashchain] });
 dotenv.config();
 
-const port = process.env.REACT_APP_MASTER_PORT;
+const port = process.env.PORT || process.env.REACT_APP_MASTER_DEFAULT_PORT;
 if (port != null) {
   server.run(parseInt(port));
 }

8d41c6c2d13e4194a70531691a87955367152ce8
kiito1000 <kiito.dev@gmail.com>
2020-10-07 21:34:59 +0900

    fix url

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,14 @@ import "./App.css";
 import { Slashchain } from "./slashchain/game";
 import { GameComponent } from "./slashchain/view/game_state";
 
-const PORT = process.env.PORT || "8000";
-const masterUrl = process.env.REACT_APP_MASTER_URL
+const masterUrl = process.env.REACT_APP_MASTER_URL;
 
 const App = Client({
   game: Slashchain,
   numPlayers: 2,
   board: GameComponent,
   multiplayer: SocketIO({
-    server: `${masterUrl}:${PORT}`,
+    server: masterUrl,
   }),
 });
 

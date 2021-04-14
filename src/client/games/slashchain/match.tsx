d92b5c512cb0c4354e81f72774954af709cac6a1
kiito1000 <kiito.dev@gmail.com>
2021-04-14 11:33:16 +0900

    refactor

--- src/client/games/slashchain/match.tsx
+++ src/client/games/slashchain/match.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect } from "react";
+import React, { FC, useCallback, useEffect } from "react";
 import { Slashchain } from "@/games";
 import { Client } from "./client";
 import { lobbyClient } from "@/client/lobby/client";
@@ -28,46 +28,57 @@ const DomComponent: FC<Props> = ({ className, matchID, player }) => (
 );
 
 export const GameMatchComponent: FC<ContainerProps> = (props) => {
-  const { matchID } = props;
   const history = useHistory();
   const [matchHistory, dispatch] = useMatchHistory();
 
+  const getMatch = useCallback(
+    (matchID: string) => lobbyClient.getMatch(Slashchain.name, matchID),
+    []
+  );
+
+  const joinMatch = useCallback(
+    (gameName: string, matchID: string, playerID: string) =>
+      lobbyClient.joinMatch(gameName, matchID, {
+        playerID,
+        playerName: playerID,
+      }),
+    []
+  );
+
+  const getPlayer = useCallback(
+    async (matchID: string) => {
+      const { gameName, players } = await getMatch(matchID);
+      const numPlayers = players.filter((player) => player.name != null).length;
+      const playerID = `${numPlayers}`;
+      const { playerCredentials } = await joinMatch(
+        gameName,
+        matchID,
+        playerID
+      );
+      return { id: playerID, credentials: playerCredentials };
+    },
+    [getMatch, joinMatch]
+  );
+
   useEffect(() => {
+    const { matchID } = props;
     const match = matchHistory[matchID];
     if (match) {
       return;
     }
 
-    lobbyClient
-      .getMatch(Slashchain.name, matchID)
-      .then(({ gameName, players }) => {
-        const numPlayers = players.filter((player) => player.name != null)
-          .length;
-        const playerID = `${numPlayers}`;
-        lobbyClient
-          .joinMatch(gameName, matchID, {
-            playerID,
-            playerName: playerID,
-          })
-          .then(({ playerCredentials }) => {
-            dispatch({
-              type: "add_match",
-              payload: {
-                matchID,
-                player: {
-                  id: playerID,
-                  credentials: playerCredentials,
-                },
-              },
-            });
-          })
-          .catch(() => {
-            history.replace("/");
-          });
+    getPlayer(matchID)
+      .then((player) => {
+        dispatch({
+          type: "add_match",
+          payload: {
+            matchID,
+            player,
+          },
+        });
       })
-      .catch(() => {
-        history.replace("/");
-      });
-  }, [matchID, history, matchHistory, dispatch]);
-  return <DomComponent {...props} player={matchHistory[matchID]} />;
+      .catch(() => history.replace("/"));
+  }, [matchHistory, dispatch, getPlayer, props, history]);
+
+  return <DomComponent {...props} player={matchHistory[props.matchID]} />;
 };

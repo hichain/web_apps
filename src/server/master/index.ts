ab20e3936c9719d2b1525cca04047cbf3b3ec4cc
kiito1000 <kiito.dev@gmail.com>
2021-04-30 16:02:33 +0900

    move files

--- src/server/master/index.ts
+++ src/server/master/index.ts
@@ -1,7 +1,7 @@
 import { Server } from "boardgame.io/server";
 import { exit } from "process";
-import { games } from "../games";
-import { envs } from "../envs";
+import { games } from "../../games";
+import { envs } from "../../envs";
 
 const port = envs?.master?.port;
 if (port == null) {

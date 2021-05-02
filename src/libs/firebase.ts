770dd13ed8e3765456e94f21b5eb851a000f26a3
kiito1000 <kiito.dev@gmail.com>
2021-05-02 21:36:27 +0900

    set envs from secrets

--- src/libs/firebase.ts
+++ src/libs/firebase.ts
@@ -4,7 +4,6 @@ import "firebase/analytics";
 const config = {
   apiKey: process.env.REACT_APP_APP_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGEBUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,

a15bd709ae57688a2b4165f9e86df7c5fa5e91da
kiito1000 <kiito.dev@gmail.com>
2021-05-05 00:42:29 +0900

    fix: initialize firebase

--- src/index.tsx
+++ src/index.tsx
@@ -8,9 +8,10 @@ import { DebugComponent } from "./client";
 import { gameComponents, GameListComponent } from "./client/games";
 import { pages, routes } from "./client/assets/routes";
 import { PageRoute } from "./page";
-import { firebase } from "@libs/firebase";
+import { initializeFirebase } from "@libs/firebase";
 
 if (process.env.NODE_ENV === "production") {
+  const firebase = initializeFirebase();
   firebase.analytics();
 }
 

08fb5a13c34184662894bf5483416465171369b1
kiito1000 <kiito.dev@gmail.com>
2021-09-04 00:21:08 +0900

    provide redux store

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,14 @@ import "@css/common.scss";
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
+import { Provider } from "react-redux";
+import { store } from "./client";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );

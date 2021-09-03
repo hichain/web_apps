3627b83d040500f6ad5ac1368cda8623b158779a
kiito1000 <kiito.dev@gmail.com>
2021-09-04 00:18:44 +0900

    refactor index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,45 +2,11 @@ import "@css/reboot.css";
 import "@css/common.scss";
 import React from "react";
 import ReactDOM from "react-dom";
-import { Redirect, Switch } from "react-router";
-import { BrowserRouter } from "react-router-dom";
-import { DebugComponent } from "./client";
-import { gameComponents, GameListComponent } from "./client/games";
-import { pages, routes } from "./client/assets/routes";
-import { PageRoute } from "./page";
+import { App } from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Switch>
-        <PageRoute
-          title={pages.gameList}
-          path={routes.gameList}
-          exact
-          component={GameListComponent}
-        />
-        <PageRoute
-          title={pages.slashchain}
-          path={routes.slashchain.index}
-          exact
-          component={gameComponents.slashchain.top}
-        />
-        <PageRoute
-          title={pages.slashchain}
-          path={routes.slashchain.match}
-          render={({ match }) => (
-            <gameComponents.slashchain.match matchID={match.params.matchID} />
-          )}
-        />
-        <PageRoute
-          title={pages.debugger}
-          path={routes.debugger}
-          exact
-          component={DebugComponent}
-        />
-        <Redirect to={routes.gameList} />
-      </Switch>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );

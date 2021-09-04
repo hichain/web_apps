65a6fa04aea0e30d9ee063d430202114905e196c
kiito1000 <kiito.dev@gmail.com>
2021-09-05 03:07:35 +0900

    sort alias

--- config-overrides.js
+++ config-overrides.js
@@ -7,13 +7,13 @@ module.exports = (config, _) => {
       ...config.resolve,
       alias: {
         ...config.resolve.alias,
-        "@": path.resolve(__dirname, "src"),
         "@strings": path.resolve(__dirname, "src/client/assets/strings"),
         "@images": path.resolve(__dirname, "src/client/assets/images"),
         "@css": path.resolve(__dirname, "src/client/assets/css"),
         "@routes": path.resolve(__dirname, "src/client/assets/routes"),
         "@games": path.resolve(__dirname, "src/games"),
         "@hooks": path.resolve(__dirname, "src/client/hooks"),
+        "@": path.resolve(__dirname, "src"),
       },
     },
   };

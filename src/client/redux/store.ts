02388ebf19fa2a3e451ae448a1838831c781232a
kiito1000 <kiito.dev@gmail.com>
2021-09-06 21:05:22 +0900

    setup redux-saga

--- src/client/redux/store.ts
+++ src/client/redux/store.ts
@@ -1,15 +1,22 @@
+import createSagaMiddleware from "@redux-saga/core";
 import { configureStore } from "@reduxjs/toolkit";
 import { gameModule } from "./modules/game";
 import { playerModule } from "./modules/player";
+import { rootSaga } from "./sagas";
 
 // Redux Design Pattern: https://react-redux.js.org/tutorials/typescript-quick-start
+
+const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
   reducer: {
     player: playerModule.reducer,
     game: gameModule.reducer,
   },
+  middleware: [sagaMiddleware],
 });
+
+sagaMiddleware.run(rootSaga);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;

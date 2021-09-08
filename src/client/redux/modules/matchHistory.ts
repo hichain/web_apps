b13f576be50a9790cd1073a5b69b3b273ad32775
kiito1000 <kiito.dev@gmail.com>
2021-09-08 21:43:49 +0900

    fix saga: lobby

--- src/client/redux/modules/matchHistory.ts
+++ src/client/redux/modules/matchHistory.ts
@@ -1,10 +1,12 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { LobbyAPI } from "boardgame.io";
 
 export type Match = {
   matchID: string;
   gameName: string;
   playerID: string;
   credentials: string;
+  detail?: LobbyAPI.Match;
 };
 
 export type MatchHistory = Match[];
@@ -18,6 +20,15 @@ export const matchHistoryModule = createSlice({
     addMatch: (state, action: PayloadAction<Match>) => {
       state.push(action.payload);
     },
+    addMatchDetail: (state, action: PayloadAction<LobbyAPI.Match[]>) => {
+      const matchList = action.payload;
+      return state.map((match) => {
+        const matchDetail = matchList.find(
+          (detail) => match.matchID === detail.matchID
+        );
+        return { ...match, detail: matchDetail };
+      });
+    },
     removeMatch: (state, action: PayloadAction<{ matchID: string }>) => {
       return state.filter((match) => match.matchID !== action.payload.matchID);
     },
@@ -27,5 +38,5 @@ export const matchHistoryModule = createSlice({
   },
 });
 
-export const { addMatch, removeMatch, clearAllMatches } =
+export const { addMatch, addMatchDetail, removeMatch, clearAllMatches } =
   matchHistoryModule.actions;
--- src/client/redux/sagas/lobby.ts
+++ src/client/redux/sagas/lobby.ts
@@ -1,18 +1,24 @@
 import { history } from "@/client/history";
 import { lobbyClient } from "@/client/lobby/client";
-import { addMatch } from "@redux/modules/matchHistory";
+import { setGameList } from "@redux/modules/gameList";
+import { addMatch, addMatchDetail } from "@redux/modules/matchHistory";
 import { createAction } from "@reduxjs/toolkit";
 import { routes } from "@routes";
-import { call, put, takeLeading } from "typed-redux-saga/dist";
+import { select } from "@utils/reduxSaga";
+import { all, call, put, takeLatest, takeLeading } from "typed-redux-saga";
 
 const actions = {
   joinMatch: "lobby/joinMatch",
+  getGames: "lobby/getGames",
+  getPlayingMatches: "lobby/getPlayingMatches",
 } as const;
 
 const actionCreators = {
   joinMatch: createAction<{ gameName: string; matchID: string }>(
     actions.joinMatch
   ),
+  getGames: createAction(actions.getGames),
+  getPlayingMatches: createAction(actions.getPlayingMatches),
 };
 
 function* joinMatchSaga(action: ReturnType<typeof actionCreators.joinMatch>) {
@@ -40,8 +46,36 @@ function* joinMatchSaga(action: ReturnType<typeof actionCreators.joinMatch>) {
   }
 }
 
-export function* lobbySagas() {
-  yield* takeLeading(actions.joinMatch, joinMatchSaga);
+function* getGamesSaga(_action: ReturnType<typeof actionCreators.getGames>) {
+  try {
+    const gameList = yield* call(lobbyClient.listGames);
+    yield* put(setGameList(gameList));
+  } catch {
+    // ignore failure
+  }
 }
 
-export const { joinMatch } = actionCreators;
+function* getPlayingMatchesSaga(
+  _action: ReturnType<typeof actionCreators.getPlayingMatches>
+) {
+  const matchHistory = yield* select((state) => state.matchHistory);
+
+  try {
+    const matchList = yield* all(
+      matchHistory.map((match) =>
+        call(lobbyClient.getMatch, match.gameName, match.matchID)
+      )
+    );
+    yield* put(addMatchDetail(matchList));
+  } catch {
+    // TODO: toast error
+  }
+}
+
+export function* lobbySagas() {
+  yield* takeLeading(actions.joinMatch, joinMatchSaga);
+  yield* takeLatest(actions.getGames, getGamesSaga);
+  yield* takeLatest(actions.getPlayingMatches, getPlayingMatchesSaga);
+}
+
+export const { joinMatch, getGames, getPlayingMatches } = actionCreators;

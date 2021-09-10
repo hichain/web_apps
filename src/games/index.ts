6c2ef4dbf149993befab9116de038c521302d8a1
kiito1000 <kiito.dev@gmail.com>
2021-09-11 01:15:10 +0900

    refactor games

--- src/games/index.ts
+++ src/games/index.ts
@@ -1,7 +1,38 @@
-import { Game } from "boardgame.io";
+import { Ctx, Game as BGIOGame } from "boardgame.io";
+import { INVALID_MOVE } from "boardgame.io/core";
 import { Slashchain } from "./slashchain";
 
 export * from "./common";
 export * from "./slashchain";
 
-export const games: Game[] = [Slashchain];
+export type InvalidMove = typeof INVALID_MOVE;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Move = (...args: any[]) => void;
+
+type GameMoves<
+  GameState extends Record<string, unknown>,
+  Moves extends Record<string, Move>
+> = {
+  [K in keyof Moves]: (
+    G: GameState,
+    ctx: Ctx,
+    ...args: Parameters<Moves[K]>
+  ) => InvalidMove | void;
+};
+
+export type Game<
+  Name extends string,
+  GameState extends Record<string, unknown>,
+  Moves extends Record<string, Move>
+> = BGIOGame<GameState> & {
+  name: Name;
+  minPlayers: number;
+  maxPlayers: number;
+  moves: GameMoves<GameState, Moves>;
+};
+
+export const games = [Slashchain];
+export const gameMap = {
+  [Slashchain.name]: Slashchain,
+} as const;

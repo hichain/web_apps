654bcb34b65c5ae5510c19754a634a9e35c7808f
kiito1000 <kiito.dev@gmail.com>
2021-09-11 21:16:38 +0900

    migrate to custom hooks

---
+++ src/client/hooks/useMatchHistory.ts
@@ -0,0 +1,14 @@
+import { SupportedGame } from "@games";
+import { useMemo } from "react";
+import { useAppSelector } from "./useAppSelector";
+
+export const useMatchHistory = (gameName: SupportedGame, matchID: string) => {
+  const matchHistory = useAppSelector((state) => state.matchHistory);
+  return useMemo(
+    () =>
+      matchHistory.find(
+        (match) => match.gameName === gameName && match.matchID === matchID
+      ),
+    [gameName, matchHistory, matchID]
+  );
+};

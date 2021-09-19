1cd3e759d233f70b4c7e7d630a0990dc6b6b9188
kiito1000 <kiito.dev@gmail.com>
2021-09-20 00:41:10 +0900

    provide theme

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom";
 import { App } from "./App";
 import { Provider } from "react-redux";
 import { store } from "./client";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { theme } from "@styles/theme";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")

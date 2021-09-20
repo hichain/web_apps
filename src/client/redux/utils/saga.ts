9e24a99307ab7eece542fcf0f79e4efebcab8b98
kiito1000 <kiito.dev@gmail.com>
2021-09-20 22:25:15 +0900

    show progress bar in running saga

--- src/client/redux/utils/saga.ts
+++ src/client/redux/utils/saga.ts
@@ -1,7 +1,7 @@
 import { Tail } from "@redux-saga/core/effects";
 import { actionCreator, RootState } from "@redux/store";
 import { AnyAction } from "redux";
-import { select as selectState, put as putState } from "typed-redux-saga";
+import { select as selectState, put as putState, call } from "typed-redux-saga";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const select = <Fn extends (state: RootState, ...args: any[]) => any>(
@@ -11,4 +11,13 @@ export const select = <Fn extends (state: RootState, ...args: any[]) => any>(
 
 export const put = (create: (creator: typeof actionCreator) => AnyAction) => {
   return putState(create(actionCreator));
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const viewAction = (worker: (...args: any[]) => unknown) => {
+  return function* (...args: unknown[]) {
+    yield* put(({ view }) => view.startAction());
+    yield* call(worker, ...args);
+    yield* put(({ view }) => view.endAction());
+  };
 };

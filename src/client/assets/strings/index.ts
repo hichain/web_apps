c7f17ada3105d6f37cc1e4c4fe277063b5088a40
kiito1000 <kiito.dev@gmail.com>
2021-09-21 22:59:56 +0900

    set as const

--- src/client/assets/strings/index.ts
+++ src/client/assets/strings/index.ts
@@ -1,28 +1,28 @@
 const app = {
   title: "HICHAIN Apps",
-};
+} as const;
 
 const games: { [game: string]: string } = {
   slashchain: "slashchain",
-};
+} as const;
 
 const responseMessages = {
   games: {
     loading: "Loading Games...",
     failure: "No games are available.",
   },
-};
+} as const;
 
 const events = {
   resetGame: "Reset",
-};
+} as const;
 
 const errors = {
   getMatch: "Failed to get the match",
   joinMatch: "Failed to join the match",
   getGames: "Failed to fetch games",
   createMatch: "Failed to create a match",
-};
+} as const;
 
 export const strings = {
   app,

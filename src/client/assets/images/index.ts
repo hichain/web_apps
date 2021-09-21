c23c51114cfd653291170efb322ceb0fb1072bc8
kiito1000 <kiito.dev@gmail.com>
2021-09-21 22:59:42 +0900

    add app icon

--- src/client/assets/images/index.ts
+++ src/client/assets/images/index.ts
@@ -1,5 +1,10 @@
 import { slashchain } from "./slashchain";
+import { ReactComponent as AppIcon } from "./app_icon.svg";
 
 export const images = {
   slashchain,
-};
+} as const;
+
+export const imageComponents = {
+  AppIcon,
+} as const;

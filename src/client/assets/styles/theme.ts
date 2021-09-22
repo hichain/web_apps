8e4a57d3430416260c7b97538a67422f20f5bc65
kiito1000 <kiito.dev@gmail.com>
2021-09-22 23:47:43 +0900

    custom material theme

--- src/client/assets/styles/theme.ts
+++ src/client/assets/styles/theme.ts
@@ -1,3 +1,16 @@
 import { createTheme } from "@mui/material";
+import { grey, deepOrange } from "@mui/material/colors";
 
-export const theme = createTheme();
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: grey[50],
+    },
+    secondary: {
+      main: deepOrange["A400"],
+    },
+    background: {
+      default: grey[50],
+    },
+  },
+});
